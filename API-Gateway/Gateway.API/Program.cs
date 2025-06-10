b30e6e4269e1126f3fd1154acd777acbe428fb8e
aledance <aferreira.17@est.ucab.edu.ve>
2025-06-10 01:49:12 -0400

    Funciona el apiGateways

---
+++ API-Gateway/Gateway.API/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Agrega servicios de Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Agrega servicios de YARP Reverse Proxy
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+// Agrega limitador de tasa
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
+var app = builder.Build();
+
+// Configura el pipeline HTTP
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRateLimiter();
+
+app.MapReverseProxy();
+app.Run();
