380dea64ae6c54dc734f7a02383d5911f7ad2add
aledance <aferreira.17@est.ucab.edu.ve>
2025-06-20 02:54:37 -0400

    se modifico para que funcionara con el keycloak

--- API-Gateway/Gateway.API/obj/Debug/net8.0/Gateway.API.AssemblyInfo.cs
+++ API-Gateway/Gateway.API/obj/Debug/net8.0/Gateway.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gateway.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+525b1a093f16a54f9d737f40d2630478fb75d585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f46a9c51f407340c54d51c9b5d3e62815955ff3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gateway.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gateway.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
